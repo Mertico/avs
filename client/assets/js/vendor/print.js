e30bab84380bf4f405067e6b73023f8228bad569
Александр Пестряков <mertico@yandex.ru>
2018-01-24 20:10:16 +0400

    Update

--- client/assets/js/vendor/print.js
+++ client/assets/js/vendor/print.js
@@ -1,22 +1,22 @@
-var profession = [
-  {
-    name: "Аналитик",
-    cost: 100
-  },
-  {
-    name: "Дизайнер",
-    cost: 120
-  },
-  {
-    name: "Разработчик",
-    cost: 250
-  },
-  {
-    name: "Верстальщик",
-    cost: 150
-  }
-]
 $(function() {
+  var profession = [
+    {
+      name: "Аналитик",
+      cost: 100
+    },
+    {
+      name: "Дизайнер",
+      cost: 120
+    },
+    {
+      name: "Разработчик",
+      cost: 250
+    },
+    {
+      name: "Верстальщик",
+      cost: 150
+    }
+  ]
   id = window.location.href.match(/\?(.*)/)[1]
   $.get("/templates/printProject.html", function( data ) {
     $.get( "/api/project/"+id )
